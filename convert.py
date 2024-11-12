a1a2482b5ae7773fbd1c040eaa2c36a1027cd05e
Jacob-Hinshaw <jrhinsha@calpoly.edu>
2024-11-12 09:48:41 -0800

    started

--- convert.py
+++ convert.py
@@ -1,1 +1,9 @@
+from typing import Optional
+
+
+def str_to_float(input1) -> Optional[float]:
+    try:
+        return float(input1)
+    except ValueError:
+        return None
 
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,18 @@
+from typing import Optional
 
+import convert
+
+if __name__ == '__main__':
+
+
+
+    def gather_numbers():
+        output_list = []
+        while True:
+            input1 = input("number?")
+            if input1 == "done":
+                return output_list
+            output_list.append(convert.str_to_float(input1))
+
+
+print(gather_numbers())
