dc1e66e24cd2734d0c539166716c1025ef581681
Jacob-Hinshaw <jrhinsha@calpoly.edu>
2024-11-12 10:12:04 -0800

    done

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-
+# this function takes an input and if it can be converted to a float, it is. Otherwise, none is returned.
 def str_to_float(input1) -> Optional[float]:
     try:
         return float(input1)
--- keyboard.py
+++ keyboard.py
@@ -2,17 +2,19 @@ from typing import Optional
 
 import convert
 
+
+# this function takes any input from the user and if it is a float, appends it to a list and returns the list
+# when the user inputs "done"
+def gather_numbers() -> list[float]:
+    output_list = []
+    while True:
+        input1 = input("number?")
+        if input1 == "done":
+            return output_list
+        output_list.append(convert.str_to_float(input1))
+
+
+
+
 if __name__ == '__main__':
-
-
-
-    def gather_numbers():
-        output_list = []
-        while True:
-            input1 = input("number?")
-            if input1 == "done":
-                return output_list
-            output_list.append(convert.str_to_float(input1))
-
-
-print(gather_numbers())
+    print(gather_numbers())
